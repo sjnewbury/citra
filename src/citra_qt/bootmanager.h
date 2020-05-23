6d7400246cc60b2f36546b460d29bb27abdb2135
Mats A <mats@mats.sh>
2020-05-23 20:01:06 +0200

    Revert "recreate mailbox to use a queue instead"
    This reverts commit 52d7676831d40a0e205af8628efe4f0460710b74.

--- src/citra_qt/bootmanager.h
+++ src/citra_qt/bootmanager.h
@@ -136,7 +136,6 @@ public:
     void PollEvents() override;
     std::unique_ptr<Frontend::GraphicsContext> CreateSharedContext() const override;
 
-    void initializeGL() override;
     void paintGL() override;
 
     void BackupGeometry();
